e886da11fb848ec7db911c9e16815032d860f94d
Anurag Roy <anuragroy@duck.com>
2023-02-18 19:29:19 +0530

    Use `Inter`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/globals.css';
+import { Inter } from '@next/font/google';
 import type { AppProps } from 'next/app';
 
+const inter = Inter({
+  subsets: ['latin'],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-inter: ${inter.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
