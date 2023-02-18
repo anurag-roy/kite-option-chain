e1d96750285b53006b1ff367b3d1731996bb7f4c
Anurag Roy <anuragroy@duck.com>
2023-02-18 20:41:58 +0530

    Optimise ticker

--- src/globals/kt.ts
+++ src/globals/kt.ts
@@ -1,63 +1,77 @@
 import env from '@/env.json';
-import { KiteTicker, TickFull, TickLtp } from 'kiteconnect-ts';
 import { readFileSync } from 'node:fs';
 import { GlobalRef } from './GlobalRef';
 import { clients, tokenMap } from './maps';
 
-const kiteTicker = new GlobalRef<KiteTicker>('myapp.kiteticker');
-if (!kiteTicker.value) {
+const processTicks = (dataView: DataView) => {
+  const numberOfPackets = dataView.getInt16(0);
+  let index = 2;
+
+  for (let i = 0; i < numberOfPackets; i++) {
+    const size = dataView.getInt16(index);
+    const token = dataView.getInt32(index);
+
+    const socketId = tokenMap.get(token);
+    if (!socketId) {
+      continue;
+    }
+    const socketClient = clients.get(socketId)!;
+    if (!socketClient) {
+      continue;
+    }
+
+    if (size === 8) {
+      socketClient?.send(
+        JSON.stringify({
+          action: 'ltp-update',
+          data: {
+            ltp: dataView.getInt32(index + 6),
+          },
+        })
+      );
+    } else if (size === 184) {
+      socketClient?.send(
+        JSON.stringify({
+          action: 'option-update',
+          data: {
+            token: token,
+            bid: dataView.getInt32(index + 70) / 100,
+            ask: dataView.getInt32(index + 130) / 100,
+          },
+        })
+      );
+    }
+
+    index = index + 2 + size;
+  }
+};
+
+const ws = new GlobalRef<WebSocket>('myapp.kiteticker');
+if (!ws.value) {
   let accessToken = undefined;
   try {
     accessToken = readFileSync('src/data/accessToken.txt', 'utf-8');
-    kiteTicker.value = new KiteTicker({
-      api_key: env.API_KEY,
-      access_token: accessToken,
-    });
+    ws.value = new WebSocket(
+      `wss://ws.kite.trade?api_key=${env.API_KEY}&access_token=${accessToken}`
+    );
 
-    kiteTicker.value.on('connect', () => {
+    ws.value.onopen = (_event) => {
       console.log('KiteTicker connected and ready to subscribe!');
-    });
+    };
 
-    kiteTicker.value.on('error', (err: any) => {
+    ws.value.onerror = (err) => {
       console.log('Error connecting to kiteTicker', err);
-    });
+    };
 
-    kiteTicker.value.on('ticks', (ticks: (TickLtp | TickFull)[]) => {
-      ticks.forEach((t) => {
-        const socketId = tokenMap.get(t.instrument_token);
-        if (!socketId) {
-          return;
-        }
-
-        const socketClient = clients.get(socketId)!;
-        if (!socketClient) {
-          return;
-        }
-
-        let message: any;
-        if (t.mode === 'ltp') {
-          message = {
-            action: 'ltp-update',
-            data: {
-              ltp: t.last_price,
-            },
-          };
-        } else {
-          message = {
-            action: 'option-update',
-            data: {
-              token: t.instrument_token,
-              bid: t.depth.buy[0].price,
-              ask: t.depth.sell[0].price,
-            },
-          };
-        }
-        socketClient?.send(JSON.stringify(message));
-      });
-    });
+    ws.value.onmessage = (event) => {
+      if (event.data instanceof ArrayBuffer && event.data.byteLength > 2) {
+        const dataView = new DataView(event.data);
+        processTicks(dataView);
+      }
+    };
   } catch (error) {
     console.log('Access token not found. Cannot initialize KiteTicker.');
   }
 }
 
-export const kt = kiteTicker.value;
+export const kt = ws.value;
